c171959693f662af805efdef018fb37c4a2c49c4
Linzhi Qi <qilinzhi@gmail.com>
2017-04-20 11:39:40 +0800

    修改知乎登录方案

--- minerva/zhihu.py
+++ minerva/zhihu.py
@@ -8,13 +8,14 @@
 
 """
 Created on 2017-04-19
-Author: qilinzhi@gmail.com
+Reference: https://github.com/xchaoinfo/fuck-login
 """
 
 import BeautifulSoup
-import cookielib
 import json
 import requests
+import time
+import datetime
 import traceback
 
 from conf import constant
@@ -27,21 +28,63 @@ class ZhihuParser(HtmlParser):
     @brief: 解析知乎的页面，获取高赞评论
     """
 
-    INDEX_URL = 'http://www.zhihu.com'
-    LOGIN_URL = 'http://www.zhihu.com/login/email'
-    CAPTCHA_URL = 'http://www.zhihu.com/captcha.gif'
+    INDEX_URL = constant.SEED_URL.ZHIHU
+    LOGIN_URL = 'https://www.zhihu.com/login/email'
+    CAPTCHA_URL = 'https://www.zhihu.com/captcha.gif?r='
 
     TIMEOUT = 20
 
     def __init__(self):
-        pass
+        if self.login('test', 'test'):
+            log.info('模拟登陆知乎成功，开始抓取')
+        else:
+            raise Exception('模拟登陆知乎失败')
 
-    def is_login(self):
+    def login(self, username, password):
         """
-        @brief: 判断用户是否登录
+        @brief: 用户登录
         """
 
-        pass
+        login_result = False 
+        session = requests.Session()
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
+            'Connection':'keep-alive'
+        }
+
+        try:
+            _xsrf = BeautifulSoup.BeautifulSoup(session.get(self.INDEX_URL, headers=headers).content).find('input', {'name': '_xsrf'})['value']
+        except Exception as e:
+            log.error('获取知乎登陆的xsrf参数失败: {}'.format(traceback.format_exc()))
+            return login_result
+
+        captcha_url = self.CAPTCHA_URL + str(int(time.time()*1000)) + '&type=login'
+        log.info('知乎登录的验证码url是: {}'.format(captcha_url))
+        captcha = raw_input('请输入验证码：')
+
+        data = {
+            '_xrsf': _xsrf,
+            'email': username,
+            'password': password,
+            'remember_me': 'true',
+            'captcha': captcha
+        }
+
+        try:
+            res = session.post(self.LOGIN_URL, headers=headers, data=data)
+            if res.status_code == 200:
+                res = res.json()
+                if isinstance(res, dict) and 'errcode' in res and res.get('errcode') and 'msg' in res:
+                    msg = res.get('msg').encode('utf8')
+                    log.error('登录知乎账户失败,失败原因是: {}'.format(msg))
+                # 登陆成功
+                elif isinstance(res, dict) and 'errcode' in res and res.get('errcode') == 0:
+                    login_result = True
+                    print res
+        except Exception as e:
+            log.error('登录知乎失败, 异常信息: {}'.format(traceback.format_exc())) 
+
+        return login_result
 
     @classmethod
     def get_info(cls, url):
@@ -49,6 +92,7 @@ class ZhihuParser(HtmlParser):
         @brief: 获取知乎的问题以及高赞的用户评论
         @return: urls: (['xxx', ['xxx']]); result: {'title': xxx, 'content': xxx}
         """
+
 
         # 保存从content中提取的结果
         urls = None
@@ -68,7 +112,6 @@ class ZhihuParser(HtmlParser):
 
 if __name__ == "__main__":
     zhihu = ZhihuParser()
-    zhihu.get_info()
 
 
 
